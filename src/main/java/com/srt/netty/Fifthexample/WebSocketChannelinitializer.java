a3b998e7063372713a20e090b36f9b5c09820641
EasyArch-Sun <3268543958@qq.com>
2021-04-22 20:20:56 +0800

    web

---
+++ src/main/java/com/srt/netty/Fifthexample/WebSocketChannelinitializer.java
@@ -0,0 +1,23 @@
+package com.srt.netty.Fifthexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+public class WebSocketChannelinitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline channelPipeline=ch.pipeline();
+
+        channelPipeline.addLast(new HttpServerCodec());
+        channelPipeline.addLast(new ChunkedWriteHandler());
+        channelPipeline.addLast(new HttpObjectAggregator(8192));
+        channelPipeline.addLast(new WebSocketServerProtocolHandler("/wa"));
+        channelPipeline.addLast(new TextWebSocketFrameHandler());
+
+    }
+}
